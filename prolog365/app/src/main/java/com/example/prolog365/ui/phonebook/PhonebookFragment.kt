4cff8b1117f368fc5b3e18192127418eaa931f75
retro3014 <retro3014@naver.com>
2023-06-30 15:47:34 +0900

    FEAT) use phonebook data

--- prolog365/app/src/main/java/com/example/prolog365/ui/phonebook/PhonebookFragment.kt
+++ prolog365/app/src/main/java/com/example/prolog365/ui/phonebook/PhonebookFragment.kt
@@ -1,14 +1,17 @@
 package com.example.prolog365.ui.phonebook
 
 import android.Manifest
+import android.content.ContentResolver
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import android.provider.ContactsContract
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
 import androidx.core.content.PermissionChecker.checkSelfPermission
@@ -35,10 +38,9 @@ class PhonebookFragment : Fragment() {
 
         _binding = FragmentPhonebookBinding.inflate(inflater, container, false)
         val root: View = binding.root
-        //getPhonebookData()
+        getPhonebookData()
 
-        //val phonebookList =
-
+        /*
         phonebookList.add(PhonebookData("가나다", "010-1234-5678"))
         phonebookList.add(PhonebookData("나다라", "010-2345-6789"))
         phonebookList.add(PhonebookData("다라마", "010-3345-6789"))
@@ -49,7 +51,7 @@ class PhonebookFragment : Fragment() {
         phonebookList.add(PhonebookData("아자차", "010-8345-6789"))
         phonebookList.add(PhonebookData("자차카", "010-9345-6789"))
         phonebookList.add(PhonebookData("차카타", "010-0345-6789"))
-
+         */
 
         binding.recyclerviewPhonebook.adapter = PhonebookAdapter(phonebookList)
         if (container != null) {
@@ -59,10 +61,8 @@ class PhonebookFragment : Fragment() {
 
         return root
     }
-    /*
+
     val permissions = arrayOf(Manifest.permission.READ_CONTACTS, Manifest.permission.CALL_PHONE)
-    lateinit var adapter:ContactsAdapter
-    var list = mutableListOf()
     var searchText = ""
     var sortText = "asc"
 
@@ -79,52 +79,94 @@ class PhonebookFragment : Fragment() {
 
 
     fun startProcess(){
-        setList()
-        setSearchListener()
-        setRadioListener()
+        getPhoneNumbers(sortText, searchText)
+        //setSearchListener()
     }
 
-    fun getPhoneNumbers(sort:String, searchName:String?):List{
-        val list = mutableListOf()
+
+    fun getPhoneNumbers(sort:String, searchName:String?){
         val addressUri = ContactsContract.Contacts.CONTENT_URI
         val phoneUri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI
 
+        //Log.d("Basic Syntax", sort + searchName)
         val projections = arrayOf(ContactsContract.CommonDataKinds.Phone.CONTACT_ID
             , ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME
             , ContactsContract.CommonDataKinds.Phone.NUMBER)
 
         var wheneClause:String? = null
-        var whereValues:Array? = null
-        if(searchName?.isNotEmpty() ?: false){
+        var whereValues:Array<String>? = null
+        if(searchName?.isNotEmpty() == true){
             wheneClause = ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + "like ?"
             whereValues = arrayOf("%$searchName")
         }
+        val optionSort = ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " $sort"
+
+        val cursor = activity?.contentResolver?.query(phoneUri, projections, wheneClause, whereValues, optionSort)
+
+        while(cursor?.moveToNext()?:false){
+            val name = cursor?.getString(1)
+            var number = cursor?.getString(2)
+            if (name!=null && number != null) {
+                val phonebookData = PhonebookData(name, number)
+                phonebookList.add(phonebookData)
+            }
+        }
+
+
+
+        /*phonebookList.add(PhonebookData("가나다", "010-1234-5678"))
+        phonebookList.add(PhonebookData("나다라", "010-2345-6789"))
+        phonebookList.add(PhonebookData("다라마", "010-3345-6789"))
+        phonebookList.add(PhonebookData("라마바", "010-4345-6789"))
+        phonebookList.add(PhonebookData("마바사", "010-5345-6789"))
+        phonebookList.add(PhonebookData("바사아", "010-6345-6789"))
+        phonebookList.add(PhonebookData("사아자", "010-7345-6789"))
+        phonebookList.add(PhonebookData("아자차", "010-8345-6789"))
+        phonebookList.add(PhonebookData("자차카", "010-9345-6789"))
+        phonebookList.add(PhonebookData("차카타", "010-0345-6789"))
+        */
     }
 
 
     fun setList(){
-        list.addAll(getPhoneNumbers(sortText, searchText))
+
     }
 
 
 
     private fun getPhonebookData(){
         if(isLower23() || isPermitted()){
+            Log.d("MyLog", "Permission Get")
             startProcess()
         }else{
-            getActivity()?.let { ActivityCompat.requestPermissions(it, permissions, 99) }
+            Log.d("MyLog", "Permission No")
+            //requestPermissions(permissions)
+            //ActivityCompat.requestPermissions(requireActivity(), permissions, 99)
+            val contract = ActivityResultContracts.RequestMultiplePermissions()
+            val activityResultLauncher = registerForActivityResult(contract) { resultMap ->
+                val isAllGranted = permissions.all {e -> resultMap[e] == true}
+                if(isAllGranted){
+                    startProcess()
+                }else{
+                    Toast.makeText(activity, "권한 승인이 필요합니다", Toast.LENGTH_LONG).show()
+                    activity?.finish()
+                }
+            }
+
+            activityResultLauncher.launch(permissions)
         }
     }
 
     fun isLower23() : Boolean{
         return Build.VERSION.SDK_INT < Build.VERSION_CODES.M
     }
-
+/*
     override fun onRequestPermissionsResult(
         requestCode: Int,
-        permissions: Array<out String>,
+        permissions: Array<String>,
         grantResults: IntArray
     ) {
+        Log.d("MyLog", "Request Permission Result")
         if (requestCode == 99) {
             var check = true
             for (grant in grantResults) {
@@ -135,13 +177,12 @@ class PhonebookFragment : Fragment() {
             }
             if (check) startProcess()
             else {
-                Toast.makeText(activity, "권한 승인이 필요합니다", Toast.LENGTH_LONG).show()
-                activity?.finish()
+
             }
         }
     }
+ */
 
-     */
 
     override fun onDestroyView() {
         super.onDestroyView()
