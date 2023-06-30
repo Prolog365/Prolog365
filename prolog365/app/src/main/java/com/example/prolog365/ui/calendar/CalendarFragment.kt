c120170c8fb4f9828b26820c416cfe7848112a34
RootPark <rmsdnjs518@gmail.com>
2023-06-30 21:09:43 +0900

    FEAT) bottom-sheet work

--- prolog365/app/src/main/java/com/example/prolog365/ui/calendar/CalendarFragment.kt
+++ prolog365/app/src/main/java/com/example/prolog365/ui/calendar/CalendarFragment.kt
@@ -8,24 +8,24 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.prolog365.R
 import com.example.prolog365.databinding.FragmentCalendarBinding
 
 class CalendarFragment : Fragment() {
 
     private var _binding: FragmentCalendarBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var fab: View
 
     val eventList = ArrayList<CalendarData>()
-
-    //private lateinit var calenderViewAdapter: CalendarRecyclerViewAdapter
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        val calendarViewModel = ViewModelProvider(this).get(CalendarViewModel::class.java)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         _binding = FragmentCalendarBinding.inflate(inflater, container, false)
-
         val root: View = binding.root
+        fab = root.findViewById(R.id.calender_floating_action_button)
 
         eventList.add(CalendarData("s", "행사1", "행사행사"))
         eventList.add(CalendarData("s", "행사2", "행사행사"))
@@ -36,15 +36,21 @@ class CalendarFragment : Fragment() {
         eventList.add(CalendarData("s", "행사7", "행사행사"))
 
         binding.eventRecyclerView.adapter = CalendarAdapter(eventList)
-        if(container!=null){
-            binding.eventRecyclerView.layoutManager = LinearLayoutManager(container.context)
-        }
+        binding.eventRecyclerView.layoutManager = LinearLayoutManager(requireContext())
 
         return root
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        fab.setOnClickListener{ v ->
+            val bottomSheetDialog = AddCalendar()
+            bottomSheetDialog.show(parentFragmentManager, "bottomSheet")
+        }
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
 }
