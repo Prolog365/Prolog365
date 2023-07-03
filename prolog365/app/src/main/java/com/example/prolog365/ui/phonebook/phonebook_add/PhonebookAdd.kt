0d7eaa9f5ff2f5810cb7105d220510855c5a4b49
retro3014 <retro3014@naver.com>
2023-07-03 13:52:47 +0900

    FEAT) phonebook add popupwindow

---
+++ prolog365/app/src/main/java/com/example/prolog365/ui/phonebook/phonebook_add/PhonebookAdd.kt
@@ -0,0 +1,16 @@
+package com.example.prolog365.ui.phonebook.phonebook_add
+
+import android.content.Context
+import com.example.prolog365.databinding.AddPhonebookBinding
+
+class PhonebookAdd {
+    companion object{
+        var _addbinding: AddPhonebookBinding? = null
+
+        private val binding get() = _addbinding!!
+
+        fun showPopupWindow(context: Context){
+
+        }
+    }
+}
