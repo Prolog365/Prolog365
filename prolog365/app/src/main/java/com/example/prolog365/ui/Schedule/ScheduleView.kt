6d500e1814bc75a862a955421052d1e972d3a43e
retro3014 <retro3014@naver.com>
2023-07-04 15:02:18 +0900

    FEAT) add schedule view layout

---
+++ prolog365/app/src/main/java/com/example/prolog365/ui/Schedule/ScheduleView.kt
@@ -0,0 +1,68 @@
+package com.example.prolog365.ui.Schedule
+
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Handler
+import android.os.Looper
+import android.view.Gravity
+import android.view.View
+import android.view.ViewGroup
+import android.widget.PopupWindow
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.prolog365.databinding.ScheduleViewBinding
+import com.example.prolog365.db.ScheduleDB
+import com.example.prolog365.db.ScheduleEntity
+import com.example.prolog365.ui.phonebook.PhonebookData
+import com.example.prolog365.ui.phonebook.PhonebookInteraction
+import com.example.prolog365.ui.phonebook.phonebook_info.PhonebookInfo
+import com.example.prolog365.ui.phonebook.phonebook_info.PhonebookInfoAdapter
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class ScheduleView{
+    companion object{
+        var scheduleBinding: ScheduleViewBinding? = null
+        val binding get() = scheduleBinding!!
+
+        fun showPopupWindow(scheduleData: ScheduleEntity, context: Context){
+            CoroutineScope(Dispatchers.IO).launch {
+                PhonebookInfo.binding.tagTextInfoPhonebook?.text = PhonebookInteraction.getFirstLetter(phonebookData.name)
+                PhonebookInfo.binding.nameTextInfoPhonebook?.text = phonebookData.name
+                PhonebookInfo.binding.phonenumberTextInfoPhonebook?.text = phonebookData.phonenumber
+                PhonebookInfo.binding.buttonMessageInfoPhonebook.setOnClickListener{
+                    PhonebookInteraction.sendMessage(phonebookData.phonenumber, context)
+                }
+                PhonebookInfo.binding.buttonPhonecallInfoPhonebook.setOnClickListener{
+                    PhonebookInteraction.sendCall(phonebookData.phonenumber, context)
+                }
+
+
+                var scheduleList = ScheduleDB.getScheduleWithPhonenumber(phonebookData.phonenumber)
+
+                val adapter = PhonebookInfoAdapter(scheduleList as ArrayList<ScheduleEntity>)
+
+                adapter.itemClick = object : PhonebookInfoAdapter.ItemClick{
+                    override fun onClick(view: View, position: Int){
+                        // Add onclick event
+                    }
+                }
+
+                PhonebookInfo.binding.scheduleRecyclerviewInfoPhonebook.adapter = adapter
+                PhonebookInfo.binding.scheduleRecyclerviewInfoPhonebook.layoutManager = LinearLayoutManager(context)
+                val mHandler = Handler(Looper.getMainLooper())
+                mHandler.post(Runnable(){
+                    val popupWindow = PopupWindow(PhonebookInfo.binding.root, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+                    popupWindow.isOutsideTouchable = true
+                    popupWindow.isFocusable = true
+                    popupWindow.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+                    popupWindow.showAtLocation(PhonebookInfo.binding.root, Gravity.CENTER, 0, 0)
+                })
+            }
+
+        }
+
+    }
+
+}
--- prolog365/app/src/main/java/com/example/prolog365/ui/gallery/GalleryAdapter.kt
+++ prolog365/app/src/main/java/com/example/prolog365/ui/gallery/GalleryAdapter.kt
@@ -20,11 +20,11 @@ import com.bumptech.glide.Glide
 class GalleryAdapter(private val context: FragmentActivity?, private val imageList: List<GalleryData>) : BaseAdapter() {
 
 
-    interface ItemClick{
-        fun onClick(view: View, position : Int)
-    }
-
-    var itemClick : ItemClick? = null
+//    interface ItemClick{
+//        fun onClick(view: View, position : Int)
+//    }
+//
+//    var itemClick : ItemClick? = null
 
     override fun getCount(): Int {
         return imageList.size
@@ -38,7 +38,10 @@ class GalleryAdapter(private val context: FragmentActivity?, private val imageLi
         return position.toLong()
     }
 
-
+    fun clickItemGallery(galleryData: GalleryData){
+        Log.d("MyLog", "Click!: " + galleryData.imageSource)
+        GalleryShow.showPopupWindow(galleryData.imageSource.toUri())
+    }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View? {
         val galleryViewModel = context?.let { ViewModelProvider(it, ViewModelProvider.NewInstanceFactory()).get(GalleryViewModel::class.java) }
@@ -48,12 +51,19 @@ class GalleryAdapter(private val context: FragmentActivity?, private val imageLi
             while(imageViewList.size <= position){
                 val idx = imageViewList.size
                 imageView = ImageView(context)
+                imageView.setOnClickListener{
+                    clickItemGallery(imageList[position])
+                }
                 imageView.layoutParams = RelativeLayout.LayoutParams(290, 290)
                 val layoutParams = imageView.layoutParams as RelativeLayout.LayoutParams
                 imageView.layoutParams = layoutParams
                 imageView.scaleType = ImageView.ScaleType.CENTER_CROP
                 Log.d("MyLog", imageList[position].imageSource)
-                imageView.setImageURI(imageList[position].imageSource.toUri())
+                //imageView.setImageURI(imageList[position].imageSource.toUri())
+                Glide.with(imageView.context)
+                    .load(imageList[position].imageSource.toUri())
+                    .into(imageView)
+
                 imageViewList.add(imageView)
             }
         }
@@ -71,9 +81,7 @@ class GalleryAdapter(private val context: FragmentActivity?, private val imageLi
 
         // Set the image for the ImageView using the Uri
 
-//            Glide.with(imageView.context)
-//                .load(imageList[position].imageSource.toUri())
-//                .into(imageView)
+
 
         //return imageView
     }
