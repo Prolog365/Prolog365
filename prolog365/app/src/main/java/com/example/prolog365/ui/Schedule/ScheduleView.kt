c9797d2ce0e11a6fb77af46f86e89549bc59ec73
retro3014 <retro3014@naver.com>
2023-07-04 15:06:58 +0900

    FIX) remove schedule view

--- prolog365/app/src/main/java/com/example/prolog365/ui/Schedule/ScheduleView.kt
+++ prolog365/app/src/main/java/com/example/prolog365/ui/Schedule/ScheduleView.kt
@@ -28,37 +28,37 @@ class ScheduleView{
 
         fun showPopupWindow(scheduleData: ScheduleEntity, context: Context){
             CoroutineScope(Dispatchers.IO).launch {
-                PhonebookInfo.binding.tagTextInfoPhonebook?.text = PhonebookInteraction.getFirstLetter(phonebookData.name)
-                PhonebookInfo.binding.nameTextInfoPhonebook?.text = phonebookData.name
-                PhonebookInfo.binding.phonenumberTextInfoPhonebook?.text = phonebookData.phonenumber
-                PhonebookInfo.binding.buttonMessageInfoPhonebook.setOnClickListener{
-                    PhonebookInteraction.sendMessage(phonebookData.phonenumber, context)
-                }
-                PhonebookInfo.binding.buttonPhonecallInfoPhonebook.setOnClickListener{
-                    PhonebookInteraction.sendCall(phonebookData.phonenumber, context)
-                }
-
-
-                var scheduleList = ScheduleDB.getScheduleWithPhonenumber(phonebookData.phonenumber)
-
-                val adapter = PhonebookInfoAdapter(scheduleList as ArrayList<ScheduleEntity>)
-
-                adapter.itemClick = object : PhonebookInfoAdapter.ItemClick{
-                    override fun onClick(view: View, position: Int){
-                        // Add onclick event
-                    }
-                }
-
-                PhonebookInfo.binding.scheduleRecyclerviewInfoPhonebook.adapter = adapter
-                PhonebookInfo.binding.scheduleRecyclerviewInfoPhonebook.layoutManager = LinearLayoutManager(context)
-                val mHandler = Handler(Looper.getMainLooper())
-                mHandler.post(Runnable(){
-                    val popupWindow = PopupWindow(PhonebookInfo.binding.root, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
-                    popupWindow.isOutsideTouchable = true
-                    popupWindow.isFocusable = true
-                    popupWindow.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-                    popupWindow.showAtLocation(PhonebookInfo.binding.root, Gravity.CENTER, 0, 0)
-                })
+//                PhonebookInfo.binding.tagTextInfoPhonebook?.text = PhonebookInteraction.getFirstLetter(phonebookData.name)
+//                PhonebookInfo.binding.nameTextInfoPhonebook?.text = phonebookData.name
+//                PhonebookInfo.binding.phonenumberTextInfoPhonebook?.text = phonebookData.phonenumber
+//                PhonebookInfo.binding.buttonMessageInfoPhonebook.setOnClickListener{
+//                    PhonebookInteraction.sendMessage(phonebookData.phonenumber, context)
+//                }
+//                PhonebookInfo.binding.buttonPhonecallInfoPhonebook.setOnClickListener{
+//                    PhonebookInteraction.sendCall(phonebookData.phonenumber, context)
+//                }
+//
+//
+//                var scheduleList = ScheduleDB.getScheduleWithPhonenumber(phonebookData.phonenumber)
+//
+//                val adapter = PhonebookInfoAdapter(scheduleList as ArrayList<ScheduleEntity>)
+//
+//                adapter.itemClick = object : PhonebookInfoAdapter.ItemClick{
+//                    override fun onClick(view: View, position: Int){
+//                        // Add onclick event
+//                    }
+//                }
+//
+//                PhonebookInfo.binding.scheduleRecyclerviewInfoPhonebook.adapter = adapter
+//                PhonebookInfo.binding.scheduleRecyclerviewInfoPhonebook.layoutManager = LinearLayoutManager(context)
+//                val mHandler = Handler(Looper.getMainLooper())
+//                mHandler.post(Runnable(){
+//                    val popupWindow = PopupWindow(PhonebookInfo.binding.root, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+//                    popupWindow.isOutsideTouchable = true
+//                    popupWindow.isFocusable = true
+//                    popupWindow.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+//                    popupWindow.showAtLocation(PhonebookInfo.binding.root, Gravity.CENTER, 0, 0)
+//                })
             }
 
         }
